39d8c3814923d1f7f3122b6b81e0aada71345d9c
Roman Nebel <im.roman.nebel@gmail.com>
2023-04-04 14:58:51 +0200

    Add success change email flow

--- processes/processContext/userChangeEmail/userChangeEmail.js
+++ processes/processContext/userChangeEmail/userChangeEmail.js
@@ -2,129 +2,141 @@ const { getDBRequest } = require("@mg/requests");
 const { sendMessageToTelegram } = require("@tg/actions/actions");
 
 function getEmail(text) {
-  return "nebel@sobakapav.ru";
+	try {
+		const lowerText = text.toLowerCase();
+		return lowerText;
+	} catch {
+		return null;
+	}
 }
 
 async function changeEmailInit({ telegramUser }) {
-  const now = Date.now();
+	const now = Date.now();
 
-  if (telegramUser) {
-    sendMessageToTelegram({
-      telegramUserId: telegramUser?.id,
-      intent: "changeEmailInit",
-      lang: "ru",
-    });
+	if (telegramUser) {
+		sendMessageToTelegram({
+			telegramUserId: telegramUser?.id,
+			intent: "changeEmailInit",
+			lang: "ru",
+		});
 
-    getDBRequest("addAction", {
-      query: {
-        userId: telegramUser?.id,
-        role: "student",
-        actionCode: 004,
-        action: "Change email request",
-        ts: now,
-      },
-    });
-  }
+		getDBRequest("addAction", {
+			query: {
+				userId: telegramUser?.id,
+				role: "student",
+				actionCode: 004,
+				action: "Change email request",
+				ts: now,
+			},
+		});
+	}
 
-  return { OK: true, newBotContext: "changeEmail" };
+	return { OK: true, newBotContext: "changeEmail" };
 }
 
 async function changeEmailFail({ telegramUser }) {
-  const now = Date.now();
+	const now = Date.now();
 
-  if (telegramUser) {
-    sendMessageToTelegram({
-      telegramUserId: telegramUser?.id,
-      intent: "changeEmailFail",
-      lang: "ru",
-    });
+	if (telegramUser) {
+		getDBRequest("addAction", {
+			query: {
+				userId: telegramUser?.id,
+				role: "student",
+				actionCode: 005,
+				action: "Change email fail",
+				ts: now,
+			},
+		});
 
-    getDBRequest("addAction", {
-      query: {
-        userId: telegramUser?.id,
-        role: "student",
-        actionCode: 005,
-        action: "Change email fail",
-        ts: now,
-      },
-    });
-  }
+		sendMessageToTelegram({
+			telegramUserId: telegramUser?.id,
+			intent: "changeEmailFail",
+			lang: "ru",
+		});
+	}
 
-  return { OK: true, newBotContext: "changeEmail" };
+	return { OK: true, newBotContext: "changeEmail" };
 }
 
 async function changeEmailError({ telegramUser }) {
-  const now = Date.now();
+	const now = Date.now();
 
-  if (telegramUser) {
-    sendMessageToTelegram({
-      telegramUserId: telegramUser?.id,
-      intent: "changeEmailError",
-      lang: "ru",
-    });
+	if (telegramUser) {
+		sendMessageToTelegram({
+			telegramUserId: telegramUser?.id,
+			intent: "changeEmailError",
+			lang: "ru",
+		});
 
-    getDBRequest("addAction", {
-      query: {
-        userId: telegramUser?.id,
-        role: "student",
-        actionCode: 005,
-        action: "Change email fail",
-        ts: now,
-      },
-    });
-  }
+		getDBRequest("addAction", {
+			query: {
+				userId: telegramUser?.id,
+				role: "student",
+				actionCode: 005,
+				action: "Change email fail",
+				ts: now,
+			},
+		});
+	}
 
-  return { OK: true, newBotContext: "changeEmail" };
+	return { OK: true, newBotContext: "changeEmail" };
 }
 
-async function changeEmailSuccess({ telegramUser }) {
-  const now = Date.now();
+async function changeEmailSuccess({ telegramUser, email }) {
+	const now = Date.now();
 
-  if (telegramUser) {
-    sendMessageToTelegram({
-      telegramUserId: telegramUser?.id,
-      intent: "changeEmailSuccess",
-      lang: "ru",
-    });
+	if (telegramUser) {
+		getDBRequest("addAction", {
+			query: {
+				userId: telegramUser?.id,
+				role: "student",
+				actionCode: 006,
+				action: "Change email success",
+				ts: now,
+			},
+		});
 
-    getDBRequest("addAction", {
-      query: {
-        userId: telegramUser?.id,
-        role: "student",
-        actionCode: 006,
-        action: "Change email success",
-        ts: now,
-      },
-    });
-  }
+		getDBRequest("updateUserInfo", {
+			query: {
+				userId: telegramUser?.id,
+			},
+			data: { email },
+		});
 
-  return { OK: true, newBotContext: undefined };
+		sendMessageToTelegram({
+			telegramUserId: telegramUser?.id,
+			intent: "changeEmailSuccess",
+			lang: "ru",
+		});
+	}
+
+	return { OK: true, newBotContext: undefined };
 }
 
 async function userChangeEmail({ telegramUser, text, botContext }) {
-  const now = Date.now();
-  const user = await getDBRequest("getUserInfo", {
-    query: { userId: telegramUser.id },
-  });
+	const now = Date.now();
+	const user = await getDBRequest("getUserInfo", {
+		query: { userId: telegramUser.id },
+	});
 
-  if (!botContext) {
-    return changeEmailInit({ telegramUser });
-  }
+	if (!botContext) {
+		return changeEmailInit({ telegramUser });
+	}
 
-  const currentEmail = user?.email;
-  const extrudedEmail = getEmail(text);
-  if (extrudedEmail) {
-    const student = await getDBRequest("getStudentInfo", {
-      query: { email: extrudedEmail },
-    });
-    if (student) {
-      changeEmailSuccess({ telegramUser });
-    } else {
-      changeEmailFail({ telegramUser });
-    }
-  } else {
-    return changeEmailError({ telegramUser });
-  }
+	const currentEmail = user?.email;
+	const extrudedEmail = getEmail(text);
+	if (extrudedEmail) {
+		const student = await getDBRequest("getStudentInfo", {
+			query: { email: extrudedEmail },
+		});
+		if (student) {
+			return changeEmailSuccess({ telegramUser, email: extrudedEmail });
+		} else {
+			return changeEmailFail({ telegramUser });
+		}
+	} else {
+		return changeEmailError({ telegramUser });
+	}
 }
 
 module.exports = userChangeEmail;
