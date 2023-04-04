1f4873cdabbc905126ba42edf70ea5321af399dd
Roman Nebel <im.roman.nebel@gmail.com>
2023-04-04 15:52:08 +0200

    Disable deprecated menu option

--- utils/telegramReplyFabric.js
+++ utils/telegramReplyFabric.js
@@ -1,66 +1,68 @@
 const { getPhrase } = require("@assets/dict/dict");
 
 const KEYBOARDS = {
-  settings({ lang }) {
-    return {
-      inline_keyboard: [
-        [
-          {
-            text: getPhrase(lang, "changeEmailButton"),
-            callback_data: "tChangeEmailButton",
-          },
-        ],
+	settings({ lang }) {
+		return {
+			inline_keyboard: [
+				[
+					{
+						text: getPhrase(lang, "changeEmailButton"),
+						callback_data: "tChangeEmailButton",
+					},
+				],
+				/*
         [
           {
             text: getPhrase(lang, "updateModulesButton"),
             callback_data: "tUpdateModulesButton",
           },
         ],
-      ],
-    };
-  },
-  changeEmailInit({ lang }) {
-    return {
-      inline_keyboard: [
-        [
-          {
-            text: getPhrase(lang, "cancelButton"),
-            callback_data: "tCancelButton",
-          },
-        ],
-      ],
-    };
-  },
-  changeEmailError({ lang }) {
-    return {
-      inline_keyboard: [
-        [
-          {
-            text: getPhrase(lang, "cancelButton"),
-            callback_data: "tCancelButton",
-          },
-        ],
-      ],
-    };
-  },
-  changeEmailFail({ lang }) {
-    return {
-      inline_keyboard: [
-        [
-          {
-            text: getPhrase(lang, "cancelButton"),
-            callback_data: "tCancelButton",
-          },
-        ],
-      ],
-    };
-  },
+        */
+			],
+		};
+	},
+	changeEmailInit({ lang }) {
+		return {
+			inline_keyboard: [
+				[
+					{
+						text: getPhrase(lang, "cancelButton"),
+						callback_data: "tCancelButton",
+					},
+				],
+			],
+		};
+	},
+	changeEmailError({ lang }) {
+		return {
+			inline_keyboard: [
+				[
+					{
+						text: getPhrase(lang, "cancelButton"),
+						callback_data: "tCancelButton",
+					},
+				],
+			],
+		};
+	},
+	changeEmailFail({ lang }) {
+		return {
+			inline_keyboard: [
+				[
+					{
+						text: getPhrase(lang, "cancelButton"),
+						callback_data: "tCancelButton",
+					},
+				],
+			],
+		};
+	},
 };
 
 function getKeyboard(lang, intent, data) {
-  if (KEYBOARDS[intent]) {
-    return KEYBOARDS[intent]({ lang, data });
-  } else return undefined;
+	if (KEYBOARDS[intent]) {
+		return KEYBOARDS[intent]({ lang, data });
+	} else return undefined;
 }
 
 module.exports = { getKeyboard };
