90bd68f0286e0ac8c5990b0f4a4fd6a84847cdbe
Roman Nebel <im.roman.nebel@gmail.com>
2023-04-14 11:00:23 +0200

    Fix cron scheduling

--- services/schedule/cron.js
+++ services/schedule/cron.js
@@ -2,10 +2,9 @@ const { log } = require("../../services/log/log");
 const cron = require("node-cron");
 const { catReport } = require("../../processes/automations/automations");
 
-const reportCat = cron.schedule("* * 11 * * 1-5", catReport);
 function init() {
-	reportCat.start();
+	cron.schedule("0 0 11 * * 1-5", catReport);
 	log.info("Schedule initialized successful");
 }
 
-module.exports = { init, reportCat };
+module.exports = { init };
