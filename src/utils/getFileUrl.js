75e87b1cce717b245d4307ede07c3c89649c92d4
Roman Nebel <im.roman.nebel@gmail.com>
2023-04-24 11:54:03 +0200

    Update telegram file downloader

--- src/utils/getFileUrl.js
+++ src/utils/getFileUrl.js
@@ -1,37 +1,49 @@
 const fetch = require("node-fetch");
+const fs = require("fs");
+const { bot } = require("../services/telegram/telegram");
+
+function sendFileToStorage({ url, name, type }) {
+	try {
+		fetch(
+			`${process.env.T_FILE_DOWNLOADER}?fileUrl=${url}&name=${name}&type=${type}`,
+			{
+				method: "GET",
+			}
+		);
+	} catch (e) {
+		log.warn("File didn't save to Google Drive\n", e);
+	}
+}
 
 async function getTelegramFileUrl(att) {
-  const fileId = att[att.length - 1]?.file_id || att?.file_id || undefined;
-  if (!fileId) {
-    return;
-  }
-  const data = await fetch(
-    `${process.env.T_FILE_DOWNLOADER}?fileId=${fileId}`,
-    {
-      method: "GET",
-    }
-  ).then((response) => {
-    return response.json();
-  });
-  const url = data?.url;
-  return url;
+	const fileId = att[att.length - 1]?.file_id || att?.file_id || undefined;
+	if (!fileId) {
+		return;
+	}
+	try {
+		const fileUrl = await bot.telegram.getFileLink(fileId);
+		sendFileToStorage({ url: fileUrl });
+		return fileUrl;
+	} catch {
+		throw new Error("");
+	}
 }
 
 async function getSlackFileUrl(file) {
-  if (!file) {
-    return;
-  }
+	if (!file) {
+		return;
+	}
 
-  const data = await fetch(
-    `${process.env.S_FILE_DOWNLOADER}?fileUrl=${file.url}&name=${file.name}&type=${file.type}`,
-    {
-      method: "GET",
-    }
-  ).then((response) => {
-    return response.json();
-  });
-  const url = data?.url;
-  return url;
+	const data = await fetch(
+		`${process.env.S_FILE_DOWNLOADER}?fileUrl=${file.url}&name=${file.name}&type=${file.type}`,
+		{
+			method: "GET",
+		}
+	).then((response) => {
+		return response.json();
+	});
+	const url = data?.url;
+	return url;
 }
 
 module.exports = { getTelegramFileUrl, getSlackFileUrl };
