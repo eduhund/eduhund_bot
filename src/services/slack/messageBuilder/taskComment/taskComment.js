2dd8e69f336bb072d74e195e12cb0e13560b5c38
Roman Nebel <im.roman.nebel@gmail.com>
2023-12-08 13:02:26 +0100

    Update taskComment message template

--- src/services/slack/messageBuilder/taskComment/taskComment.js
+++ src/services/slack/messageBuilder/taskComment/taskComment.js
@@ -1,11 +1,12 @@
 function taskComment({ from, to, message }) {
-	const { email, firstName, lastName, taskId, taskName } = from;
+	const { email, firstName, lastName, moduleName, taskId, taskName } = from;
   const { text } = message
 
   const name = `${firstName || ""} ${lastName || ""}`
-  const taskLink = `https://my.eduhund.com/${taskId.substr(0, 3)}/${taskId.substr(3, 2)}/${taskId.substr(5)}`
+	const moduleId = taskId.substr(0, 3)
+  const taskLink = `https://test.eduhund.com/${moduleId}/${taskId.substr(3, 2)}/${taskId.substr(5)}`
 	const informerMessage = `${name}: ${text}`;
-	const userInfo = `${name} (${email}) спрашивает свой вопрос в задаче *${taskName}* [<${taskLink}|${taskId}>]`;
+	const userInfo = `${name} (${email}) спрашивает вопрос в задаче *${taskName}* модуля ${moduleName || moduleId} [<${taskLink}|${taskId}>]`;
 
 	return {
 		channel: to.channelId,
