7dc1ffddbf470cc312667a3de662ff78506b5bdd
Roman Nebel <im.roman.nebel@gmail.com>
2025-01-19 19:51:58 +0100

    Update taskComment text

--- src/services/slack/messageBuilder/taskComment/taskComment.js
+++ src/services/slack/messageBuilder/taskComment/taskComment.js
@@ -1,36 +1,41 @@
 function taskComment({ from, to, message }) {
-	const { email, firstName, lastName, moduleName, taskId, taskName } = from;
-  const { text } = message
+  const { email, firstName, lastName, moduleName, taskId, taskName } = from;
+  const { text } = message;
 
-  const name = `${firstName || ""} ${lastName || ""}`
-	const moduleId = taskId.substr(0, 3)
-  const taskLink = `https://test.eduhund.com/${moduleId}/${taskId.substr(3, 2)}/${taskId.substr(5)}`
-	const informerMessage = `${name}: ${text}`;
-	const userInfo = `${name} (${email}) спрашивает вопрос в задаче *${taskName}* модуля ${moduleName || moduleId} [<${taskLink}|${taskId}>]`;
+  const name = `${firstName || ""} ${lastName || ""}`;
+  const moduleId = taskId.substr(0, 3);
+  const taskLink = `https://my.eduhund.com/${moduleId}/${taskId.substr(
+    3,
+    2
+  )}/${taskId.substr(5)}`;
+  const informerMessage = `${name}: ${text}`;
+  const userInfo = `${name} (${email}) оставил комментарий к задаче *${taskName}* модуля ${
+    moduleName || moduleId
+  } [<${taskLink}|${taskId}>]`;
 
-	return {
-		channel: to.channelId,
-		text: informerMessage,
-		blocks: [
-			{
-				type: "context",
-				elements: [
-					{
-						type: "mrkdwn",
-						text: userInfo,
-					},
-				],
-			},
-			{
-				type: "section",
-				text: {
-					type: "plain_text",
-					text: text || " ",
-					emoji: true,
-				},
-			},
-		],
-	};
+  return {
+    channel: to.channelId,
+    text: informerMessage,
+    blocks: [
+      {
+        type: "context",
+        elements: [
+          {
+            type: "mrkdwn",
+            text: userInfo,
+          },
+        ],
+      },
+      {
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: text || " ",
+          emoji: true,
+        },
+      },
+    ],
+  };
 }
 
 module.exports = { taskComment };
